74951c17373fa1c80a4c4287a0cd1900c3b0b7a2
rubalaganesh-colla <rubalaganesh@gmail.com>
2025-08-23 17:32:51 +0530

    Add files via upload

---
+++ activity26.py
@@ -0,0 +1,7 @@
+a=[1,5,7,8,9]
+b=[2,3,4,6]
+c=a
+print(a is c)
+print(a is b)
+print(a is not c)
+
---
+++ activity28.py
@@ -0,0 +1,21 @@
+x=int(input("enter a number:"))
+if(type (x) is int):
+    print("true")
+else:
+    print("false")
+
+
+y=float(input("enter a number:"))
+if(type (y) is not float):
+    print("true")
+else:
+    print("false")
+
+x=int(input("enter a number:"))
+y=int(input("enter a number:"))
+if(x is y):
+    print("true")
+else:
+    print("false")          
+
+
