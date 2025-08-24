66a75c750dd9f87fa407e80ad740c168175672a7
rubalaganesh-colla <rubalaganesh@gmail.com>
2025-08-24 17:28:49 +0530

    Add files via upload

---
+++ activity34.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the value of x: "))
+y=int(input("Enter the value of y: "))
+
+x = x+y
+y = x-y
+x = x-y
+
+print("After swapping, value of x:",x)
+print("After swapping, value of y:",y)  
