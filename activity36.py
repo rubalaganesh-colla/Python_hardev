fb8ce170731db4c0cd71e6ad0bdcc5329d8b506d
rubalaganesh-colla <rubalaganesh@gmail.com>
2025-08-30 17:45:27 +0530

    Add files via upload

---
+++ activity36.py
@@ -0,0 +1,55 @@
+print("select you vichile")
+print("1.bike")
+print("2.car")
+
+choice = int( input("enter you choice:"))
+if ( choice==1):
+    print("what type of bike")
+    print("1.scoote\n")
+    print("2.bike\n")
+
+    choice2 = int(input("enter choice 2: "))
+if  choice2==1:
+    print("select if you choice scoote")
+    print("select if you choice bike")
+    
+    
+choice = int( input("enter you choice:"))
+if ( choice==2):
+    print("what type of car")
+    print("1.auto\n")
+    print("2.car\m")
+
+choice3 = int(input("enter you choice3:"))
+if choice3==1:
+    print("select if you choice auto ")
+    print("select if you choice car")
+
+else:
+    print("selected wrong choice")
+
+
+ 
+    
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
