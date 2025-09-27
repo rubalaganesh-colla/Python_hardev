3b56c3014b50fcec0a9b35126a7d68a1d3d9d9fa
rubalaganesh-colla <rubalaganesh@gmail.com>
2025-09-27 17:20:47 +0530

    Add files via upload

---
+++ activity69.py
@@ -0,0 +1,14 @@
+for x in range(30):
+    if x % 20 == 0:
+        print("tom")
+
+    elif x % 15 == 0:
+        pass 
+    elif x % 5 == 0:
+        print("ram")
+    elif x % 2 == 0:
+        print("sam")
+    else:
+        print(x)      
+        
+        
