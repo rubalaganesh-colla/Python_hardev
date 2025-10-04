feffee2645c172e0f3757550e43fe67b3a0f4a1f
rubalaganesh-colla <rubalaganesh@gmail.com>
2025-10-04 17:47:37 +0530

    Add files via upload

---
+++ activity84.py
@@ -0,0 +1,8 @@
+import math 
+print('the value of pi is :',math.pi)
+str(math.ceil(5.5)) +', ' + str(math.floor(5.5))
+x= int(input('enter a number:'))
+y= int(input('enter another number:'))
+print('the value of x after calculating the sine of x is: ' +str(math.sin(x)))
+print('the value of x after calculating the cosine of x is: ' +str(math.cos(x)))
+print('the value of x after calculating the tangent of x is: ' +str(math.tan(x)))
