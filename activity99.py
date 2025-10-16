3fa67c169028439379e241fd50e1607bcddae99f
rubalaganesh-colla <rubalaganesh@gmail.com>
2025-10-16 18:25:26 +0530

    Add files via upload

---
+++ activity99.py
@@ -0,0 +1,8 @@
+setx ={"green", "blue", "red"}
+sety ={"python", "java", "c++"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nUnion of two sets:")
+setz = setx.union(sety)
+print(setz)
