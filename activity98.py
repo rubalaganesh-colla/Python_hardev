019987b5c5ae63b66ca6d1cb71157ba39855981e
rubalaganesh-colla <rubalaganesh@gmail.com>
2025-10-16 18:19:41 +0530

    Add files via upload

---
+++ activity98.py
@@ -0,0 +1,14 @@
+my_set = {1,2,3,4,5,6,7,8,9}
+print(my_set)
+my_set = {1.0,"hello",(1,2,3,)}
+print(my_set)
+my_set = {1,2,3,4,3,2}
+print(my_set)
+my_set = set([1,2,3,4,5,6,7,8,9])
+print(my_set,"\n")
+num_set = {1,2,3,4,5,6,7,8,9}
+print("the original set is:")
+print(num_set)
+num_set.pop()
+print("the set after removing an element is:")
+print(num_set,"\n")
